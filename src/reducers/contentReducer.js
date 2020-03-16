dbd0e5cbc4d74fd7fd5c99e570cd89cd2c5e9ad6
Huang-Yin Chung <in1102@livemail.tw>
2020-03-16 11:42:12 +0800

    refactor: remove console.log

--- src/reducers/contentReducer.js
+++ src/reducers/contentReducer.js
@@ -1,7 +1,6 @@
 import { FETCH_SEARCH } from '../actions/types';
 
 export default function(state = {}, action) {
-	console.log('contentReducer: action', action);
 	switch (action.type) {
 		case FETCH_SEARCH:
 			return action.payload || false;
